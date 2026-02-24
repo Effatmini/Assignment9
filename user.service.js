3bb173db46c59fbc0f3f6d5f67be381300fad856
Effatmini <effatmini@gmail.com>
2026-02-24 15:16:01 +0200

    Update user.service.js

--- user.service.js
+++ user.service.js
@@ -13,7 +13,8 @@ export const createUser = async ({ username, email, password }) => {
   return await user.save();
 };
 
-// Reg Validate
+Reg Validate
+
 const LoginSchema = Joi.object({
   email: Joi.string()
     .pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
@@ -30,39 +31,40 @@ const LoginSchema = Joi.object({
       "string.pattern.base": "Password must be at least 8 characters and include uppercase, lowercase, number, and special character",
     }),
 })
-// joi validate
 
-// export const validateLogin = (data) => {
-//   const schema = Joi.object({
-//     email: Joi.string()
-//       .email()
-//       .required()
-//       .messages({
-//         "string.empty": "Email is required",
-//         "string.email": "Email must be valid joi Validate--",  
-//       }),
-//     password: Joi.string()
-//       .min(6)
-//       .required()
-//       .messages({
-//         "string.empty": "Password is required",
-//         "string.min": "Password must be at least 6 characters",
-//       }),
-//        username: Joi.string()
-//     .optional()
+joi validate
+
+export const validateLogin = (data) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email()
+      .required()
+      .messages({
+        "string.empty": "Email is required",
+        "string.email": "Email must be valid joi Validate--",  
+      }),
+    password: Joi.string()
+      .min(6)
+      .required()
+      .messages({
+        "string.empty": "Password is required",
+        "string.min": "Password must be at least 6 characters",
+      }),
+       username: Joi.string()
+    .optional()
   
   
-//   });
+  });
   
 
 
 
-//   const { error } = schema.validate(data, { abortEarly: false });
-//   if (error) {
-//     const errors = error.details.map(d => d.message);
-//     throw new Error(JSON.stringify(errors));
-//   }
-// };
+  const { error } = schema.validate(data, { abortEarly: false });
+  if (error) {
+    const errors = error.details.map(d => d.message);
+    throw new Error(JSON.stringify(errors));
+  }
+};
 export const loginUser = async ({ email, password }) => {
   const user = await User.findOne({ email }).select("+password");
   if (!user) throw new Error("Invalid email or password");
@@ -76,4 +78,5 @@ console.log("Password in DB:", user.password);
 
   return user;
 };
+
 
