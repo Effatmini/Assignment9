ed29d4e140a651beb7c66a0c46b1345430212a08
Effatmini <effatmini@gmail.com>
2026-02-24 15:10:01 +0200

    Add files via upload

--- user.service.js
+++ user.service.js
@@ -1,5 +1,6 @@
 import User from "./user.model.js";
 import bcrypt from "bcryptjs";
+import Joi from "joi";
 
 export const createUser = async ({ username, email, password }) => {
   const existingUser = await User.findOne({ email });
@@ -12,7 +13,50 @@ export const createUser = async ({ username, email, password }) => {
   return await user.save();
 };
 
+// Reg Validate
+const schema = Joi.object({
+  password: Joi.string()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}$"))
+    .required()
+    .messages({
+      "string.empty": "Password is required",
+      "string.pattern.base": "Password must be at least 6 characters and include uppercase, lowercase, and number",
+    }),
+});
 
+// joi validate
+
+// export const validateLogin = (data) => {
+//   const schema = Joi.object({
+//     email: Joi.string()
+//       .email()
+//       .required()
+//       .messages({
+//         "string.empty": "Email is required",
+//         "string.email": "Email must be valid joi Validate--",  
+//       }),
+//     password: Joi.string()
+//       .min(6)
+//       .required()
+//       .messages({
+//         "string.empty": "Password is required",
+//         "string.min": "Password must be at least 6 characters",
+//       }),
+//        username: Joi.string()
+//     .optional()
+  
+  
+//   });
+  
+
+
+
+//   const { error } = schema.validate(data, { abortEarly: false });
+//   if (error) {
+//     const errors = error.details.map(d => d.message);
+//     throw new Error(JSON.stringify(errors));
+//   }
+// };
 export const loginUser = async ({ email, password }) => {
   const user = await User.findOne({ email }).select("+password");
   if (!user) throw new Error("Invalid email or password");
